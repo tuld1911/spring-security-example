982cd228de31e9d3ac6b6720607e68f5711edd0f
tuld1911 <dinhtul43@gmail.com>
2017-03-08 13:05:59 +0700

    Add files via upload

---
+++ SpringSecurity/src/main/java/com/tuld/MvcConfig.java
@@ -0,0 +1,16 @@
+package com.tuld;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter{
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("index");
+		registry.addViewController("/index").setViewName("index");
+		registry.addViewController("/home").setViewName("home");
+		registry.addViewController("/login").setViewName("login");
+	}
+}
